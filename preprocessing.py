290ef1d8663cdd7774c43b10c13d9bca7acba8b7
EmanueleCosenza <emanuele.cosenza23@gmail.com>
2022-02-04 00:59:05 +0100

    General cleanup, parameter dict, gnn edge feature nn fix

---
+++ preprocessing.py
@@ -0,0 +1,320 @@
+import os
+import muspy
+from itertools import product
+import pypianoroll as pproll
+import time
+from tqdm.auto import tqdm
+import numpy as np
+import torch
+import random
+
+
+class MIDIPreprocessor():
+
+    def __init__():
+        pass
+
+    def preprocess_dataset(self, dir, early_exit=None):
+        pass
+
+    def preprocess_file(self, f):
+        pass
+
+
+# Todo: to config file (or separate files)
+MAX_SIMU_NOTES = 16 # 14 + SOS and EOS
+
+PITCH_SOS = 128
+PITCH_EOS = 129
+PITCH_PAD = 130
+DUR_SOS = 96
+DUR_EOS = 97
+DUR_PAD = 98
+
+MAX_DUR = 96
+
+# Number of time steps per quarter note
+# To get bar resolution -> RESOLUTION*4
+RESOLUTION = 8
+NUM_BARS = 1
+
+
+def preprocess_file(filepath, dest_dir, num_samples):
+
+    saved_samples = 0
+
+    print("Preprocessing file " + filepath)
+
+    # Load the file both as a pypianoroll song and a muspy song
+    # (Need to load both since muspy.to_pypianoroll() is expensive)
+    try:
+        pproll_song = pproll.read(filepath, resolution=RESOLUTION)
+        muspy_song = muspy.read(filepath)
+    except Exception as e:
+        print("Song skipped (Invalid song format)")
+        return 0
+
+    # Only accept songs that have a time signature of 4/4 and no time changes
+    for t in muspy_song.time_signatures:
+        if t.numerator != 4 or t.denominator != 4:
+            print("Song skipped ({}/{} time signature)".
+                            format(t.numerator, t.denominator))
+            return 0
+
+    # Gather tracks of pypianoroll song based on MIDI program number
+    drum_tracks = []
+    bass_tracks = []
+    guitar_tracks = []
+    strings_tracks = []
+
+    for track in pproll_song.tracks:
+        if track.is_drum:
+            #continue
+            track.name = 'Drums'
+            drum_tracks.append(track)
+        elif 0 <= track.program <= 31:
+            track.name = 'Guitar'
+            guitar_tracks.append(track)
+        elif 32 <= track.program <= 39:
+            track.name = 'Bass'
+            bass_tracks.append(track)
+        else:
+            # Tracks with program > 39 are all considered as strings tracks
+            # and will be merged into a single track later on
+            strings_tracks.append(track)
+
+    # Filter song if it does not contain drum, guitar, bass or strings tracks
+    #if not guitar_tracks \
+    if not drum_tracks or not guitar_tracks \
+            or not bass_tracks or not strings_tracks:
+        print("Song skipped (does not contain drum or "
+                "guitar or bass or strings tracks)")
+        return 0
+
+    # Merge strings tracks into a single pypianoroll track
+    strings = pproll.Multitrack(tracks=strings_tracks)
+    strings_track = pproll.Track(pianoroll=strings.blend(mode='max'),
+                                 program=48, name='Strings')
+
+    combinations = list(product(drum_tracks, bass_tracks, guitar_tracks))
+    #combinations = list(product(bass_tracks, guitar_tracks))
+
+    # Single instruments can have multiple tracks.
+    # Consider all possible combinations of drum, bass, and guitar tracks
+    for i, combination in enumerate(combinations):
+
+        print("Processing combination", i+1, "of", len(combinations))
+
+        # Process combination (called 'subsong' from now on)
+        drum_track, bass_track, guitar_track = combination
+        tracks = [drum_track, bass_track, guitar_track, strings_track]
+        #bass_track, guitar_track = combination
+        #tracks = [bass_track, guitar_track, strings_track]
+
+        pproll_subsong = pproll.Multitrack(
+            tracks=tracks,
+            tempo=pproll_song.tempo,
+            resolution=RESOLUTION
+        )
+        muspy_subsong = muspy.from_pypianoroll(pproll_subsong)
+
+        tracks_notes = [track.notes for track in muspy_subsong.tracks]
+
+        # Obtain length of subsong (maximum of each track's length)
+        length = 0
+        for notes in tracks_notes:
+            track_length = max(note.end for note in notes) if notes else 0
+            length = max(length, track_length)
+        length += 1
+
+        # Add timesteps until length is a multiple of RESOLUTION
+        length = length if length%(RESOLUTION*4) == 0 \
+                            else length + (RESOLUTION*4-(length%(RESOLUTION*4)))
+
+
+        tracks_tensors = []
+        tracks_activations = []
+
+        # Todo: adapt to velocity
+        for notes in tracks_notes:
+
+            # Initialize encoder-ready track tensor
+            # track_tensor: (length x max_simu_notes x 2 (or 3 if velocity))
+            # The last dimension contains pitches and durations (and velocities)
+            # int16 is enough for small to medium duration values
+            track_tensor = np.zeros((length, MAX_SIMU_NOTES, 2), np.int16)
+
+            track_tensor[:, :, 0] = PITCH_PAD
+            track_tensor[:, 0, 0] = PITCH_SOS
+            track_tensor[:, :, 1] = DUR_PAD
+            track_tensor[:, 0, 1] = DUR_SOS
+
+            # Keeps track of how many notes have been stored in each timestep
+            # (int8 imposes that MAX_SIMU_NOTES < 256)
+            notes_counter = np.ones(length, dtype=np.int8)
+
+            # Todo: np.put_along_axis?
+            for note in notes:
+                # Insert note in the lowest position available in the timestep
+
+                t = note.time
+
+                if notes_counter[t] >= MAX_SIMU_NOTES-1:
+                    # Skip note if there is no more space
+                    continue
+
+                track_tensor[t, notes_counter[t], 0] = note.pitch
+                dur = min(MAX_DUR, note.duration)
+                track_tensor[t, notes_counter[t], 1] = dur-1
+                notes_counter[t] += 1
+
+            # Add end of sequence token
+            track_tensor[np.arange(0, length), notes_counter, 0] = PITCH_EOS
+            track_tensor[np.arange(0, length), notes_counter, 1] = DUR_EOS
+
+            # Get track activations, a boolean tensor indicating whether notes
+            # are being played in a timestep (sustain does not count)
+            # (needed for graph rep.)
+            activations = np.array(notes_counter-1, dtype=bool)
+
+            tracks_tensors.append(track_tensor)
+            tracks_activations.append(activations)
+
+        # (#tracks x length x max_simu_notes x 2 (or 3))
+        subsong_tensor = np.stack(tracks_tensors, axis=0)
+
+        # (#tracks x length)
+        subsong_activations = np.stack(tracks_activations, axis=0)
+
+
+        # Slide window over 'subsong_tensor' and 'subsong_activations' along the
+        # time axis (2nd dimension) with the stride of a bar
+        # Todo: np.lib.stride_tricks.as_strided(song_proll)
+        for i in range(0, length-NUM_BARS*RESOLUTION*4+1, RESOLUTION*4):
+
+            # Get the sequence and its activations
+            seq_tensor = subsong_tensor[:, i:i+NUM_BARS*RESOLUTION*4, :]
+            seq_acts = subsong_activations[:, i:i+NUM_BARS*RESOLUTION*4]
+            seq_tensor = np.copy(seq_tensor)
+            seq_acts = np.copy(seq_acts)
+
+            if NUM_BARS > 1:
+                # Skip sequence if it contains more than one bar of consecutive
+                # silence in at least one track
+                bars = seq_acts.reshape(seq_acts.shape[0], NUM_BARS, -1)
+                bars_acts = np.any(bars, axis=2)
+
+                if 1 in np.diff(np.where(bars_acts == 0)[1]):
+                    continue
+            else:
+                # In the case of just 1 bar, skip it if all tracks are silenced
+                bar_acts = np.any(seq_acts, axis=1)
+                if not np.any(bar_acts):
+                    continue
+
+            # Randomly transpose the pitches of the sequence (-5 to 6 semitones)
+            # Not considering pad, sos, eos tokens
+            # Not transposing drums/percussions
+            shift = np.random.choice(np.arange(-5, 7), 1)
+            cond = (seq_tensor[1:, :, :, 0] != PITCH_PAD) &                     \
+                   (seq_tensor[1:, :, :, 0] != PITCH_SOS) &                     \
+                   (seq_tensor[1:, :, :, 0] != PITCH_EOS)
+            #cond = (seq_tensor[:, :, :, 0] != PITCH_PAD) &                     \
+            #       (seq_tensor[:, :, :, 0] != PITCH_SOS) &                     \
+            #       (seq_tensor[:, :, :, 0] != PITCH_EOS)
+            non_perc = seq_tensor[1:, ...]
+            #non_perc = seq_tensor
+            non_perc[cond, 0] += shift
+
+            # Save sample (seq_tensor and seq_acts) to file
+            curr_sample = str(num_samples + saved_samples)
+            sample_filepath = os.path.join(dest_dir, curr_sample)
+            np.savez(sample_filepath, seq_tensor=seq_tensor, seq_acts=seq_acts)
+
+            saved_samples += 1
+
+
+    print("File preprocessing finished. Saved samples:", saved_samples)
+    print()
+
+    return saved_samples
+
+
+
+# Total number of files: 116189
+# Number of unique files: 45129
+def preprocess_dataset(dataset_dir, dest_dir, num_files=45129, early_exit=None):
+
+    files_dict = {}
+    seen = 0
+    tot_samples = 0
+    not_filtered = 0
+    finished = False
+
+    print("Starting preprocessing")
+
+    progress_bar = tqdm(range(early_exit)) if early_exit is not None else tqdm(range(num_files))
+    start = time.time()
+
+    # Visit recursively the directories inside the dataset directory
+    for dirpath, dirs, files in os.walk(dataset_dir):
+
+        # Sort alphabetically the found directories
+        # (to help guess the remaining time)
+        dirs.sort()
+
+        print("Current path:", dirpath)
+        print()
+
+        for f in files:
+
+            seen += 1
+
+            if f in files_dict:
+                # Skip already seen file
+                files_dict[f] += 1
+                continue
+
+            # File never seen before, add to dictionary of files
+            # (from filename to # of occurrences)
+            files_dict[f] = 1
+
+            # Preprocess file
+            filepath = os.path.join(dirpath, f)
+            n_saved = preprocess_file(filepath, dest_dir, tot_samples)
+
+            tot_samples += n_saved
+            if n_saved > 0:
+                not_filtered += 1
+
+            progress_bar.update(1)
+
+            # Todo: also print # of processed (not filtered) files
+            #       and # of produced sequences (samples)
+            print("Total number of seen files:", seen)
+            print("Number of unique files:", len(files_dict))
+            print("Total number of non filtered songs:", not_filtered)
+            print("Total number of saved samples:", tot_samples)
+            print()
+
+            # Exit when a maximum number of files has been processed (if set)
+            if early_exit != None and len(files_dict) >= early_exit:
+                finished = True
+                break
+
+        if finished:
+            break
+
+    end = time.time()
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("Preprocessing completed in (h:m:s): {:0>2}:{:0>2}:{:05.2f}"
+              .format(int(hours),int(minutes),seconds))
+
+
+if __name__ == "__main__":
+
+    dataset_dir = './data/lmd_matched/'
+    dest_dir = './data/preprocessed_1bar'
+
+    preprocess_dataset(dataset_dir, dest_dir)
